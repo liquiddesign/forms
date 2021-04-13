8a59980edd6dca5098e4d552512dd2d2b22810d3
Oldrich <oldrich@lqd.cz>
2021-04-13 18:28:20 +0200

    mutations fix

--- src/assets/filemanager/config/config.php
+++ src/assets/filemanager/config/config.php
@@ -12,7 +12,12 @@ $container = App\Bootstrap::boot()->createContainer();
 $url = $container->getByType(\Nette\Http\Request::class)->getUrl();
 $baseUrl = $url->getScheme() . '://' . $url->getHost();
 $rootUrl = \dirname($url->getPath(), $rootLevel) === '/' ? '/' :  \dirname($url->getPath(), $rootLevel) . '/';
-$relativePath = \str_repeat('../', $rootUrl === '/' ? $rootLevel - 2 : $rootLevel - 1);
+$relativePath = \str_repeat('../', $rootUrl === '/' ? $rootLevel : $rootLevel - 1);
+
+var_dump($baseUrl);
+var_dump($rootUrl);
+var_dump($relativePath);
+die();
 
 $configuration = $container->getByType(\Forms\Forms::class)->getWysiwygConfiguration('filemanager');
 $directory = $configuration['directory'];
