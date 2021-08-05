60ff326c8481115ee961433cc86a63ff4d785299
honza952 <honza@lqd.cz>
2021-08-05 11:46:37 +0200

    filemanager fileupload filename

--- src/assets/filemanager/config/config.php
+++ src/assets/filemanager/config/config.php
@@ -279,13 +279,13 @@ $config = array(
 	//Show or not language selection feature in filemanager
 	'show_language_selection' => true,
 	//active or deactive the transliteration (mean convert all strange characters in A..Za..z0..9 characters)
-	'transliteration' => false,
+	'transliteration' => true,
 	//convert all spaces on files name and folders name with $replace_with variable
-	'convert_spaces' => false,
+	'convert_spaces' => true,
 	//convert all spaces on files name and folders name this value
 	'replace_with' => "_",
 	//convert to lowercase the files and folders name
-	'lower_case' => false,
+	'lower_case' => true,
 
 	//Add ?484899493349 (time value) to returned images to prevent cache
 	'add_time_to_img' => false,
